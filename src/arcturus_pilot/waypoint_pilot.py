f8476ef62c22edf93ebe7711b424ebd4ffb5ba4e
ryan10145 <chang.ryan10145@gmail.com>
2022-03-08 00:59:24 -0500

    Update waypoint pilot to use position setpoint instead of mission

--- src/arcturus_pilot/waypoint_pilot.py
+++ src/arcturus_pilot/waypoint_pilot.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 import rospy
-from mavros_msgs.msg import State, WaypointReached, Waypoint, WaypointList
-from mavros_msgs.srv import CommandBool, SetMode, WaypointPush
+import tf
+from mavros_msgs.msg import State, Waypoint
+from mavros_msgs.srv import CommandBool, SetMode
 from sensor_msgs.msg import NavSatFix, Imu
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Point, Quaternion, Pose
 from pymavlink import mavutil
 
 from arcturus_pilot.msg import Waypoint
 
 from six.moves import xrange
+
+ACCEPTANCE_RADIUS = 0.2
 
 class WaypointPilot():
     def __init__(self):
@@ -22,7 +25,6 @@ class WaypointPilot():
 
         self.waypoints = []
         self.current_order = 0
-        self.mission_item_reached = -1
 
     def run(self):
         rospy.init_node('pilot', anonymous=True)
@@ -32,7 +34,6 @@ class WaypointPilot():
         try:
             rospy.wait_for_service('mavros/cmd/arming', service_timeout)
             rospy.wait_for_service('mavros/set_mode', service_timeout)
-            rospy.wait_for_service('mavros/mission/push', service_timeout)
             rospy.loginfo("ROS services are up")
         except rospy.ROSException:
             rospy.logerr('failed to connect to services')
@@ -40,7 +41,6 @@ class WaypointPilot():
         
         self.set_arming_srv = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
         self.set_mode_srv = rospy.ServiceProxy('mavros/set_mode', SetMode)
-        self.push_waypoints_srv = rospy.ServiceProxy('mavros/mission/push', WaypointPush)
         
         rospy.Subscriber('mavros/state', State, self.state_callback)
         rospy.Subscriber('mavros/imu/data', Imu, self.imu_callback)
@@ -51,20 +51,18 @@ class WaypointPilot():
             for key in ['state', 'imu', 'global_pos', 'local_pos']
         }
 
-        rospy.Subscriber('mavros/mission/reached', WaypointReached, self.mission_item_reached_callback)
-
         rospy.Subscriber('waypoint', Waypoint, self.waypoint_callback)
 
+        self.set_local_setpoint = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=5)
+
         self.wait_for_topics(60)
-        self.set_mode("Auto", 5)
+        self.set_mode("Guided", 5)
         self.set_arm(True, 5)
 
         rate = rospy.Rate(10) # frequency in Hz at which we can update next waypoint
         while not rospy.is_shutdown():
-            # send everything in queue to the controller
-            if len(self.waypoints != 0):
-                self.push_waypoints(self.waypoints, 10)
-                self.waypoints = []
+            # constantly update our current setpoint
+            self.send_setpoint()
             rate.sleep()
 
         self.set_arm(False, 5)
@@ -117,51 +115,32 @@ class WaypointPilot():
         if not self.sub_topics_ready['global_position']:
             self.sub_topics_ready['global_position'] = True
 
+    # subscribes to the local position of the boat and updates the next
+    # waypoint if the local position is within acceptance radius of current setpoint
     def local_position_callback(self, data):
         self.local_position = data
         if not self.sub_topics_ready['local_pos']:
             self.sub_topics_ready['local_pos'] = True
+        
+        if len(self.waypoints == 0):
+            return
+        
+        curr_setpoint = self.waypoints[0]
+        dist_sq = (self.local_position.pose.x - curr_setpoint[0]) ** 2 + (self.local_position.pose.y - curr_setpoint[1]) ** 2
+        if dist_sq < ACCEPTANCE_RADIUS ** 2:
+            self.waypoints.pop(0)
+            self.current_order += 1
+            self.send_setpoint()
 
+    # subscribes to incoming waypoints and adds them to a list in
+    # the correct order to be processed
     def waypoint_callback(self, waypoint):
         adjusted_order = waypoint.order - self.current_order
         if adjusted_order < 0:
             return
         while len(self.waypoints < adjusted_order):
             self.waypoints.append(None)
-        self.waypoints[adjusted_order] = self.create_waypoint(waypoint.x, waypoint.y)
-
-    def create_waypoint(waypoint_x, waypoint_y):
-        """
-        Frame values:
-            uint8 FRAME_GLOBAL = 0
-            uint8 FRAME_LOCAL_NED = 1
-            uint8 FRAME_MISSION = 2
-            uint8 FRAME_GLOBAL_REL_ALT = 3
-            uint8 FRAME_LOCAL_ENU = 4
-
-        Command values:
-            uint16 NAV_WAYPOINT = 16
-            uint16 NAV_LOITER_UNLIM = 17
-            uint16 NAV_LOITER_TURNS = 18
-            uint16 NAV_LOITER_TIME = 19
-            uint16 NAV_RETURN_TO_LAUNCH = 20
-            uint16 NAV_LAND = 21
-            uint16 NAV_TAKEOFF = 22
-
-        https://mavlink.io/en/messages/common.html#MAV_CMD_NAV_WAYPOINT
-        Params for waypoint:
-            1: hold time in seconds
-            2: accept radius in m
-            3: pass radius in m
-            4: yaw n deg (use NaN to use current system)
-        """
-        return Waypoint(frame=1, command=16, is_current=False, autocontinue=True, param1=0.0, param2=0.25,
-            param3=0, param4=float('nan'), x_lat=waypoint_x, y_lon=waypoint_y, z_alt=0)
-
-    def mission_item_reached_callback(self, data):
-        if self.mission_item_reached != data.wp_seq:
-            rospy.loginfo("mission item reached: {0}".format(data.wp_seq))
-            self.mission_item_reached = data.wp_seq
+        self.waypoints[adjusted_order] = (waypoint.x, waypoint.y, waypoint.heading)
 
     def set_arm(self, arm, timeout):
         rospy.loginfo("setting FCU arm: {0}".format(arm))
@@ -204,26 +183,23 @@ class WaypointPilot():
                 rate.sleep()
             except rospy.RosException as e:
                 rospy.logerr(e)
+    
+    # takes the current setpoint and sends it to the FCU
+    # needs to be called at minimum 2 Hz
+    def send_setpoint(self):
+        if len(self.waypoints) == 0:
+            return
+        waypoint = self.waypoints[0]
+        waypoint_x, waypoint_y, waypoint_heading = waypoint[0], waypoint[1], waypoint[2]
+        rospy.loginfo("setting setpoint: {0, 1, 2}".format(waypoint_x, waypoint_y, waypoint_heading))
 
-    def push_waypoints(self, waypoints, timeout):
-        rospy.loginfo("pushing waypoints")
-        loop_freq = 1 # Hz
-        rate = rospy.Rate(loop_freq)
-        for i in xrange(timeout * loop_freq):
-            try:
-                res = self.push_waypoints_srv(self.current_order, waypoints)
-                if res.success:
-                    self.current_order += len(waypoints)
-                    break
-                else:
-                    rospy.logerr("failed to send waypoints")
-            except rospy.ServiceException as e:
-                rospy.logerr(e)
-        
-            try:
-                rate.sleep()
-            except rospy.RosException as e:
-                rospy.logerr(e)
+        pose = PoseStamped()
+        pose.header.stamp = rospy.now()
+        pose.header.frame_id = '/local_origin'
+        pose.pose = Pose()
+        pose.pose.position = Point(x=waypoint_x, y=waypoint_y, z=0)
+        pose.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, waypoint_heading))
+        self.set_local_setpoint.publish(pose)
 
 if __name__ == '__main__':
     try:
