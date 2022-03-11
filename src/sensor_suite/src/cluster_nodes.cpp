24f4710e23fff9b15e66e4b9625de50f52ab240d
hturner08 <hmturner@mit.edu>
2022-03-10 22:40:40 -0500

    Implemented region-growing point cloud segmentation using pcl

--- src/sensor_suite/src/cluster_nodes.cpp
+++ src/sensor_suite/src/cluster_nodes.cpp
@@ -1,6 +1,13 @@
+#include <pcl/features/normal_3d.h>
+#include <pcl/point_types.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/search/search.h>
+#include <pcl/segmentation/region_growing.h>
+
+#include <iostream>
+#include <vector>
+
 #include "geometry.h"
-#include "pcl/point_types.h"
-#include "pcl_conversions/pcl_conversions.h"
 #include "pcl_ros/point_cloud.h"
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
@@ -15,14 +22,35 @@ class ClusterNode {
   ros::Subscriber pcl_sub_;
   ros::Publisher pcl_pub_;
   tf::TransformListener listener_;
-  sensor_msgs::PointCloud2::Ptr buffer_;
+
+  pcl::PCLPointCloud<pcl::PointXYZ> buffer_;
+  pcl::IndicesPtr indices;
+  pcl::search::Search<pcl::PointXYZ>::Ptr tree_;
+  pcl::PointCloud<pcl::Normal>::Ptr normals_;
+
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
+  pcl::SACSEgmentation<pcl::PointXYZ> seg;
+  pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
+  pcl::PointCloud<pcl::PointXYZ>::iterator iter;
 
  private:
   vector e, viewDirection;
   vector w, u, v;
 
  public:
-  ClusterNode(ros::NodeHandle n) : nh_(n), {
+  ClusterNode(ros::NodeHandle n) : nh_(n), indices(new std::vector<int>) {
+    // calculate basis vectors for projections
+    vector up = newVector(0, 0, 1);
+    w = scale(1 / qsize(viewDirection), viewDirection);
+    u = scale(1 / qsize(qcross(up, w)), qcross(up, w));
+    v = qcross(w, u);
+    normal_estimator.setSearchMethod(tree);
+    normal_estimator.setInputCloud(cloud);
+    normal_estimator.setKSearch(50);
+    reg.setSmoothnessThreshold(3.0 / 180.0 * M_PI);
+    reg.setCuravtureThreshold(1.0);
+
+    // Initiate subscribers and publishers
     pcl_sub_ = nh_.subscribe("/sensor_suite/raw_cloud", 1,
                              &ClusterNode::pcCallback, this);
     pcl_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(
@@ -30,29 +58,34 @@ class ClusterNode {
     e = newVector(800 * f, 100 * f, 0);
     viewDirection = newVector(-1, 0, 0);
 
-    // calculate basis vectors
-    vector up = newVector(0, 0, 1);
-    w = scale(1 / qsize(viewDirection), viewDirection);
-    u = scale(1 / qsize(qcross(up, w)), qcross(up, w));
-    v = qcross(w, u);
-    buffer_.reset(new sensor_msgs::PointCloud2);
+    // Clear point_cloud pointers
+    // TODO: Move to initialization List
+    buffer_.reset(new pcl::PointCloud<pcl::PointXYZ>);
+    normals_.reset(new pcl::PointCloud<pcl::Normal>);
+    tree_.reset(new pcl::search::KdTree<pcl::PointXYZ>);
     buffer_->header.frame_id = "world";
   }
 
   void ClusterNode::pcCallback(
       const sensor_msgs::PointCloud2ConstPtr& pcl_msg) {
-    pcl::PCLPointCloud<pcl::PointXYZ> cloud;
-    pcl::fromROSMsg(*input, cloud);
-    pcl::PCLPointCloud2 clustered_cloud;
+    pcl::fromROSMsg(*input, *buffer);
+    pcl::removeNaNFromPointCloud(*buffer, *indices);
 
-    pcl::SACSEgmentation<pcl::PointXYZ> seg;
+    reg.setInputCloud(buffer_);
+    reg.SetIndices(indices_);
 
-    for (auto& point : pcl_msg->points) {
-      vector point = {point.x, point.y, point.z};
-      pixel px = ClusterNode::projectPointToImage(
-          point, 1080,
-          1920, )  // Image sized based on
-                   // https://support.stereolabs.com/hc/en-us/articles/360007395634-What-is-the-camera-focal-length-and-field-of-view-
+    normal_estimator.compute(*normals);
+    std::vector<pcl::PointIndices> clusters;
+    reg.extract(clusters);
+    for (int i = 0; i < clusters.size(); i++) {
+      for (int j = 0; j < clusters[i].indices.size(); j++) {
+        // vector point = {point.x, point.y, point.z};
+        // pixel px = ClusterNode::projectPointToImage(
+        //     point, 1080,
+        //     1920, )  // Image sized based on
+        // https://support.stereolabs.com/hc/en-us/articles/360007395634-What-is-the-camera-focal-length-and-field-of-view-
+        // TO-DO Checkbbounding box to determine how to assign cluster
+      }
     }
     return;
   }
@@ -60,7 +93,7 @@ class ClusterNode {
   // Based of this piazza post: https://piazza.com/class/kt40btbwj942gr?cid=528
   vector ClusterNode::projectPointToPlane(vector point) {
     // center image frame
-    vector normalVector = qcross(u, v);  // Todo: Move to main render function
+    vector normalVector = qcross(u, v);
 
     float scalar =
         qdot(normalVector, e) / qdot(qsubtract(point, e), normalVector);
