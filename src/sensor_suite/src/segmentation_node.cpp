40256d759d1f43218c93c3d0cd4010468cc22769
hmturner <hmturner@mit.edu>
2022-04-05 19:32:56 -0400

    Cleaned up segmentation code

--- src/sensor_suite/src/segmentation_node.cpp
+++ src/sensor_suite/src/segmentation_node.cpp
@@ -1,3 +1,5 @@
+#include "math.h"
+
 #include "cv_bridge/cv_bridge.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Image.h"
@@ -10,6 +12,12 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
+
+// TODO
+// Refactor so each new segmentation node extends base class 
+// Add enums for colors for more readable array access
+
+
 class SegmentationNode {
  protected:
   ros::NodeHandle nh_;
@@ -23,7 +31,7 @@ class SegmentationNode {
 
  public:
   SegmentationNode(ros::NodeHandle n) : nh_(n) {
-    img_sub_ = nh_.subscribe("/zed/rgb/image_rect_color", 1,
+    img_sub_ = nh_.subscribe("/zed2i/rgb/image_rect_color", 1,
                              &SegmentationNode::imgCallback, this);
     img_pub_ = nh_.advertise<sensor_msgs::Image>("/sensor_suite/image", 1);
     box_pub_ = nh_.advertise<sensor_suite::LabeledBoundingBox2DArray>(
@@ -130,9 +138,9 @@ class SegmentationClusterNode {
   boost::shared_ptr<Sync> sync_;
 
  public:
-  SegmentationClusterNode(ros::NodeHandle n) : nh_(n), img_sub_(nh_, "/zed/rgb/image_rect_color", 1),
-                                               depth_sub_(nh_, "/zed/depth/depth_registered", 1),
-                                               info_sub_(nh_, "/zed/rgb/camera_info", 1)
+  SegmentationClusterNode(ros::NodeHandle n) : nh_(n), img_sub_(nh_, "/zed2i/zed_node/rgb/image_rect_color", 1),
+                                               depth_sub_(nh_, "/zed2i/zed_node/depth/depth_registered", 1),
+                                               info_sub_(nh_, "/zed2i/zed_node/rgb/camera_info", 1)
                                                {
     object_pub_ = nh_.advertise<sensor_suite::ObjectArray>("/sensor_suite/objects", 1);
     box_pub_ = nh_.advertise<sensor_suite::LabeledBoundingBox2DArray>("/sensor_suite/bounding_boxes", 1);
@@ -164,91 +172,70 @@ class SegmentationClusterNode {
     // Use dilation on Image:
     cv::dilate(img_, img_, element);
     // Colour thresholding with each color
-    cv::Mat thresh_red;
-    cv::Mat thresh_green;
-    cv::Mat thresh_yellow;
+    cv::Mat thresholds[3];
     cv::inRange(img_, cv::Scalar(0, 100, 100), cv::Scalar(10, 255, 255),
-                thresh_red);
+                thresholds[0]);
     cv::inRange(img_, cv::Scalar(40, 100, 100), cv::Scalar(70, 255, 255),
-                thresh_green);
+                thresholds[1]);
     cv::inRange(img_, cv::Scalar(20, 100, 100), cv::Scalar(30, 255, 255),
-                thresh_yellow);
+                thresholds[2]);
     // Get multiple contours on Image:
-    std::vector<std::vector<cv::Point>> contours_red;
-    std::vector<std::vector<cv::Point>> contours_green;
-    std::vector<std::vector<cv::Point>> contours_yellow;
-    cv::findContours(thresh_red, contours_red, CV_RETR_EXTERNAL,
-                     CV_CHAIN_APPROX_SIMPLE);
-    cv::findContours(thresh_green, contours_green, CV_RETR_EXTERNAL,
-                     CV_CHAIN_APPROX_SIMPLE);
-    cv::findContours(thresh_yellow, contours_yellow, CV_RETR_EXTERNAL,
-                     CV_CHAIN_APPROX_SIMPLE);
-    cv::drawContours(img_, contours_red, -1, cv::Scalar(0, 0, 255), 2);
-    cv::drawContours(img_, contours_green, -1, cv::Scalar(0, 255, 0), 2);
-    cv::drawContours(img_, contours_yellow, -1, cv::Scalar(255, 255, 0), 2);
+    std::vector<std::vector<cv::Point>> colored_contours[3]; 
+    cv::Scalar colors[3] = {cv::Scalar(0, 0, 255), cv::Scalar(0, 255, 0), cv::Scalar(255, 0, 0)};
+    for(int i = 0; i < 3; i++){
+      cv::findContours(thresholds[i], colored_contours[i], CV_RETR_EXTERNAL,
+                      CV_CHAIN_APPROX_SIMPLE);
+    }
     // Create bounding boxes and objects for each contour
     sensor_suite::LabeledBoundingBox2DArray box_array_;
     sensor_suite::ObjectArray object_array_;
-    for (int i = 0; i < contours_red.size(); i++) {
-      cv::Rect rect = cv::boundingRect(contours_red[i]);
-      cv::rectangle(img_, rect, cv::Scalar(0, 0, 255), 2);
-      sensor_suite::LabeledBoundingBox2D box;
-      box.x = rect.x;
-      box.y = rect.y;
-      box.width = rect.width;
-      box.height = rect.height;
-      box.label = 1;  // TODO: Change to Enum
-      box_array_.boxes.push_back(box);
-      sensor_suite::Object object;
-      cv::Point3d point = cam_model_.projectPixelTo3dRay(cv::Point2d(rect.x + rect.width / 2, rect.y + 3*rect.height / 4));
-      object.point.x = point.x;
-      object.point.y = point.y;
-      object.point.z = point.z;
-      object.label = 1;
-      object.header = img_msg->header;
-      object_array_.objects.push_back(object);
-    }
-    for (int i = 0; i < contours_green.size(); i++) {
-      cv::Rect rect = cv::boundingRect(contours_green[i]);
-      cv::rectangle(img_, rect, cv::Scalar(0, 255, 0), 2);
-      sensor_suite::LabeledBoundingBox2D box;
-      box.x = rect.x;
-      box.y = rect.y;
-      box.width = rect.width;
-      box.height = rect.height;
-      box.label = 2;
-      box_array_.boxes.push_back(box);
-      sensor_suite::Object object;
-      cv::Point3d point = cam_model_.projectPixelTo3dRay(cv::Point2d(rect.x + rect.width / 2, rect.y + 3*rect.height / 4));
-      object.point.x = point.x;
-      object.point.y = point.y;
-      object.point.z = point.z;
-      object.label = 2;
-      object.header = img_msg->header;
-      object_array_.objects.push_back(object);
-    }
-    for (int i = 0; i < contours_yellow.size(); i++) {
-      cv::Rect rect = cv::boundingRect(contours_yellow[i]);
-      cv::rectangle(img_, rect, cv::Scalar(0, 255, 255), 2);
-      sensor_suite::LabeledBoundingBox2D box;
-      box.x = rect.x;
-      box.y = rect.y;
-      box.width = rect.width;
-      box.height = rect.height;
-      box.label = 3;
-      box_array_.boxes.push_back(box);
-      sensor_suite::Object object;
-      cv::Point3d point = cam_model_.projectPixelTo3dRay(cv::Point2d(rect.x + rect.width / 2, rect.y + 3*rect.height / 4));
-      object.point.x = point.x;
-      object.point.y = point.y;
-      object.point.z = point.z;
-      object.label = 3;
-      object.header = img_msg->header;
-      object_array_.objects.push_back(object);
+    for(int i = 0; i < 3; i++){
+      for(int j = 0; j < colored_contours[i].size(); j++){
+      cv::Rect rect = cv::boundingRect(colored_contours[i][j]);
+        if(isValidBox(&rect)){
+          cv::rectangle(img_, rect, colors[i], 4);
+          sensor_suite::LabeledBoundingBox2D box;
+          box.x = rect.x;
+          box.y = rect.y;
+          box.width = rect.width;
+          box.height = rect.height;
+          box.label = i + 1;  // TODO: Change to Enum
+          box_array_.boxes.push_back(box);
+          sensor_suite::Object object;
+          cv::Point3d point = cam_model_.projectPixelTo3dRay(cv::Point2d(rect.x + rect.width / 2, rect.y + 3*rect.height / 4));
+          object.point.x = point.x;
+          object.point.y = point.y;
+          object.point.z = point.z;
+          object.label = i + 1;
+          object.header = img_msg->header;
+          object_array_.objects.push_back(object);
+        }else{
+          // colored_contours[i].erase(colored_contours[i].begin() + j);
+          // j--;
+        }
+      }
+      // cv::drawContours(img_, colored_contours[i], -1, colors[i], 4);
     }
     box_pub_.publish(box_array_);
     object_pub_.publish(object_array_);
     img_pub_.publish(cv_ptr->toImageMsg());
+  }
+
+  bool isValidBox(cv::Rect* rect){
+    if(rect->width*rect->height < 50){
+      return false;
+    }
+    if(rect->width > rect->height){
+      if (rect->width/rect->height > 2){
+        return false; 
+      }
+    }
+    else{
+      if(rect->height/rect->width > 2){
+        return false; 
+      }
+    }
+    return true;
   }
 };
 
