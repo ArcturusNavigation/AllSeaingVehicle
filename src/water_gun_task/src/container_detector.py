81fe456c9810e46194e0624cbb374cd6d6cf9fa6
hturner08 <hmturner@mit.edu>
2022-06-21 05:38:54 -0700

    started water gun task detection

--- src/water_gun_task/src/container_detector.py
+++ src/water_gun_task/src/container_detector.py
@@ -3,116 +3,91 @@
 import rospy
 import numpy as np
 import cv2
+import tf
+import message_filters
 from cv_bridge import CvBridge, CvBridgeError
 
 from sensor_msgs.msg import Image
-
-class ContainerDetection():
-	"Class that detects container with ball in it using color segmentation"
+from std_msgs.msg import Float32
 
 
-	def __init__(self):
-		#self.container_pub = # TODO  rospy.Publisher()
-		self.debug_pub = rospy.Publisher("/debug_img", Image, queue_size = 10)
-		#self.image_sub = # TODO rospy.Subscriber(,,image_callback)
-		self.bridge = CvBridge()
+class WaterGunController:
 
-	
-	def image_callback(self, image_msg):
-		# Apply your imported color segmentation function (cd_color_segmentation) to the image msg here
-		# From your bounding box, take the center pixel on the bottom
-		# (We know this pixel corresponds to a point on the ground plane)
-		# publish this pixel (u, v) to the /relative_container_px topic; the homography transformer will
-		# convert it to the boat frame.
+    def __init__(self):
+        self.image_sub = message_filters.Subscriber(
+            "/zed2i/zed_node/rgb/image_rect_color", Image)
+        self.info_sub = message_filters.Subscriber(
+            "/zed2i/zed_node/rgb/camera_info", Image)
+		self.yaw_pub = rospy.Publisher("/watergun/yaw", Float32, queue_size=1)
+		self.pitch_pub = rospy.Publisher("/watergun/pitch", Float32, queue_size=1)
+		self.img_pub = rospy.Publisher("/watergun/debug", Image, queue_size=1)
+        ts = message_filters.ApproximateTimeSynchronizer(
+            [self.image_sub, self.info_sub], 10, 0.1, allow_headerless=True)
+        ts.registerCallback(self.callback)
+		self.listener = tf.TransformListener()
+		self.watergun_tf = self.listener.lookupTransform(
+		    '/watergun', '/camera', rospy.Time(0))
 
-		#################################
-		# detect the container and publish its
-		# pixel location in the image.
-		#################################
+    def callback(self, image_msg, info_msg):
+		try:
+			image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+		except CvBridgeError as e:
+            center_2d,frame = self.find_center(image)
+		if center_2d is not None: 
+			center_3d = self.project_to_3d(center_2d, info_msg)
+			watergun_pos = self.watergun_tf.transform.translation
+			yaw, pitch = self.calc_angles(center_3d, watergun_pos)
+			self.yaw_pub.publish(yaw)
+			self.pitch_pub.publish(pitch)
+			self.img_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
 
-		image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+	def calc_angles(self, center_3d, watergun_pos):
+		x = center_3d.x - watergun_pos.x
+		y = center_3d.y - watergun_pos.y
+		z = center_3d.z - watergun_pos.z
+		angle_1 = np.arctan2(y, x)
+		angle_2 = np.arctan2(z, np.sqrt(x**2 + y**2))
+		return angle_1, angle_2
 
-		#Change these bounds to get correct color
-		lower_bound = (0, 100, 10)
-		upper_bound = (50, 255, 255)
-		template = [lower_bound, upper_bound]
+	def find_center(self, img):
+		kernel = np.ones((5, 5), np.uint8)
+		img_erosion = cv2.erode(img, kernel, iterations=1)
+		img_dilation = cv2.dilate(img_erosion, kernel, iterations=2)
+		frame = img_dilation
+		# Convert BGR to HSV
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		# Blue color boundaries
+		lower_bound = np.array([108, 150, 0])
+		upper_bound = np.array([140, 255, 255])
 
-		#Get bounding box using color segmentation
-		bb = cd_color_segmentation(image, template)
-		#cone_base = ((bb[0,0]+bb[1,0])/2, bb[1][1])
-
-		# Used for cones, will change for container for boat
-		# cone_msg = ConeLocationPixel()
-		# cone_msg.u=(bb[0][0]+bb[1][0])/2
-		# cone_msg.v = bb[1][1]
-		debug_img = cv2.rectangle(image, bb[0], bb[1], (0, 255, 0), 2) #adds box onto image
-		debug_msg = self.bridge.cv2_to_imgmsg(debug_img, "bgr8")
-		self.debug_pub.publish(debug_msg)
-		#self.cone_pub.publish(cone_msg)
-
-def image_print(img):
-	"""
-	Helper function to print out images, for debugging. Pass them in as a list.
-	Press any key to continue.
-	"""
-	cv2.imshow("image", img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-def container_color_segmentation(img, template=None):
-	"""
-	Implement the cone detection using color segmentation algorithm
-	Input:
-		img: np.3darray; the input image with a cone to be detected. BGR.
-		template_file_path; List of two tuples: (low values, high values) in where each value is (hue, sat, val)
-	Return:
-		bbox: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
-				(x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
-	"""
-	########## YOUR CODE STARTS HERE ##########
-	x = y = w = h = 0
-	# image_print(img) #prints image
-	# bounding values: change for different lighting conditions in HSV
-	if template is not None:
-		lower_bounds = template[0]
-		upper_bounds = template[1]
-	else:
-		lower_bounds = (5, 200, 100)
-		upper_bounds = (35, 255, 255)
-
-	#Convert bgr to hsv    
-	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #converts bgr to hsv
-	#Creates binary mask image
-	mask = cv2.inRange(hsv_img, lower_bounds, upper_bounds) 
-	image_print(mask)
-	
-	#Erode and clean up image
-	element = np.ones((5, 5), np.uint8) #kernel for erosion/dilation
-	erosion_dst = cv2.erode(mask, element, iterations=1) #shrinks mask area down
-	mask = cv2.dilate(erosion_dst, element, iterations=1) #increases mask area
-	
-
-	hsv, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #finding areas were masks exist
-	cnt = None
-	max_area = -1
-	# finding largest contour in mask
-	for i in contours:
-		area = i.shape[0]
-		if area > max_area:
-			cnt = i
-			max_area = area
-	if cnt is not None:
-		x, y, w, h = cv2.boundingRect(cnt) # creates rectangle around largest contour
-		cv2.rectangle(img, (x, y), (x+w,y+h), (255, 0, 0), 2) #adds box onto image
-	#image_print(img)
-	bounding_box = ((x,y),(x+w,y+h))
-
-	########### YOUR CODE ENDS HERE ###########
-
-	# Return bounding box
-	return bounding_box
+		mask = cv2.inRange(hsv, lower_bound, upper_bound)
+		contours, _ = cv2.findContours(
+			mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		bounding_boxes = []
+		for contour in contours:
+			# c = max(contours, key = cv2.contourArea)
+			x, y, w, h = cv2.boundingRect(contour)
+			if w > 40 and h > 40:
+				bounding_boxes.append(((x, y), (x+w, y+h)))
+		# cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+		min_x = np.inf
+		max_x = -np.inf
+		min_y = np.inf
+		max_y = -np.inf
+		for bounding_box in bounding_boxes:
+			x, y = bounding_box[0]
+			w, h = bounding_box[1][0] - x, bounding_box[1][1] - y
+			min_x = min(min_x, x)
+			max_x = max(max_x, x+w)
+			min_y = min(min_y, y)
+			max_y = max(max_y, y+h)
+			# cv2.rectangle(frame, bounding_box[0], bounding_box[1], (0,0,255), 2)
+		cv2.rectangle(frame, (min_x, min_y),
+						(max_x, max_y), (0, 0, 255), 5)
+		return ((min_x+max_x)/2, (min_y+max_y)/2), frame
 
 
 if __name__ == '__main__':
-	img = cv2.imread("roboatContainer.png") # insert image path
-	container_color_segmentation(img)
+    rospy.init_node('water_gun_controller')
+    water_gun_controller = WaterGunController()
+    rospy.spin()
