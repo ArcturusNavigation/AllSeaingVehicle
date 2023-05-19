e384b950779953b75242005cc3a7d0869e6d32fd
jacobrod2456 <jacobrod@mit.edu>
2023-05-18 20:02:59 -0400

    changed controller skeleton node

--- src/controller_suite/scripts/controller.py
+++ src/controller_suite/scripts/controller.py
@@ -7,29 +7,43 @@ from geometry_msgs.msg import Twist
 
 disToObstacle = 1
 
-def callback(msg): 
+feedback_linear_velocity = 0
+feedback_angular = 0
+
+commanded_velocity = 0
+
+def stateCallback(msg): 
   rospy.loginfo(msg.data) #prints on terminal
 
   # Controlller takes in boat data from msg that it subscribes to
   
   ################# Implement Controller Here #################
   
-  
-  
-  
-  
-  
+  feedback_linear_velocity= msg.data.x
+  feedback_angular = msg.data.w
+ 
   ##############################################################
-  #Some PWM Command
-  pwm = 100
+  
+
+def callback2(msg): 
+  rospy.loginfo(msg.data) #prints on terminal
+
+  # Controlller takes in boat data from msg that it subscribes to
+  
+  ################# Implement Controller Here #################
+  
   pub.publish(move)
+  
+  
   
 
 rospy.init_node('controller_node')
-sub = rospy.Subscriber('/mavros', Twist, callback) #We subscribe to the boats velocity
+
+sub = rospy.Subscriber('/mavros/velocity_unstamped', Twist, stateCallback) #We subscribe to the boats velocity
+sub = rospy.Subscriber('/mavros', Twist, commandCallback) #We subscribe to the boats velocity
+
 pub = rospy.Publisher('/controller/output_pwm', Twist, queue_size=2)
 rate = rospy.Rate(2)
-move = Twist()
 
 
 rospy.spin()
