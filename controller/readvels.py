ce662b160cd840e9d091cfe1c5afe66a1b4121a3
Toya Takahashi <toyatakahashi522@gmail.com>
2023-05-21 13:32:50 -0400

    merge commit

--- controller/readvels.py
+++ controller/readvels.py
@@ -1,3 +1,7 @@
+import rospy
+from geometry_msgs.msg import TwistStamped
+from mavros_msgs.msg import OverrideRCIn
+
 # PID CONTROLLER 
 # INPUT: TARGET FORWARD(M/S) AND ANGULAR VELOCITY(RAD/S)
 # OUTPUT: TWO THRUSTER FORCE VALUES (N)
@@ -29,25 +33,24 @@ KDW = 0.0
 W_ERROR_BOUND = 0.05
 #---------------
 
-
 class Controller(object): 
 	def __init__(self): 
 		self.Insight = Boat()
 		self.pidv = PID(KPV, KIV, KDV, SETPOINT_V)
 		self.pidw = PID(KPW, KIW, KDW, SETPOINT_W)
+		self.M = 0
+		self.R = 1
+		
 	def cycle(self): 
-		M = 0
-		R = 1
-		# while(True): # change to a condition for stopping controller
-		M += self.pidv.compute(self.Insight.get_dy()) # compute new T1+T2
-		if M > MAX_THRUST: 
-			M = MAX_THRUST
-		if M < 0: 
-			M = 0
+		self.M += self.pidv.compute(self.Insight.get_dy()) # compute new T1+T2
+		if self.M > MAX_THRUST: 
+			self.M = MAX_THRUST
+		if self.M < 0: 
+			self.M = 0
 		difw = self.Insight.get_w() - SETPOINT_W	
 		if abs(difw) > W_ERROR_BOUND: 
-			R += self.pidw.compute(self.Insight.get_w()) # compute new T1/T2
-		self.Insight.thrust(R,M) # send thrust command
+			self.R += self.pidw.compute(self.Insight.get_w()) # compute new T1/T2
+		self.Insight.thrust(self.R,self.M) # send thrust command
 
 class Boat(object):
 	def __init__(self):
@@ -57,7 +60,7 @@ class Boat(object):
 		return self.dy ## find current forward velocity
 	def get_w(self): 
 		return self.w ## find current angular velocity 
-	def thrusttoPWM(R, M): 
+	def PWM(R, M): 
 		pass ## implement toya node, send PWM to thrusters
 		
 	
@@ -91,7 +94,42 @@ class PID(object):
 	def get_kie(self):
 		return self.ki*self.integral_error
 
-def main():
-	c = Controller()
-	c.cycle()
-main()
+
+def velocity_callback(data):
+    # Process the velocity data received from the Pixhawk
+    linear_velocity_x = data.twist.linear.x
+    linear_velocity_y = data.twist.linear.y
+    linear_velocity_z = data.twist.linear.z
+    angular_velocity_x = data.twist.angular.x
+    angular_velocity_y = data.twist.angular.y
+    angular_velocity_z = data.twist.angular.z
+
+    # Implement your control algorithm here
+    # For example, let's assume a simple proportional controller for the linear velocity along the x-axis
+    target_velocity = 1.0  # Desired linear velocity
+    k_p = 0.5  # Proportional gain
+
+    # Calculate the control command
+    error = target_velocity - linear_velocity_x
+    control_command = k_p * error
+
+    # Convert the control command to PWM values
+
+
+    # Create a PWM command message
+    pwm_msg = OverrideRCIn()
+    pwm_msg.channels = [left, right]  # Initialize all channels to zero
+
+    # Set the appropriate channel value for your actuators
+    # ... Modify the channel indices and values based on your actuators and requirements
+
+    # Publish the PWM command
+    pwm_pub.publish(pwm_msg)
+
+if __name__ == '__main__':
+    # c = Controller()
+	# c.cycle()
+    rospy.init_node('velocity_controller')
+    rospy.Subscriber('/mavros/local_position/velocity_local', TwistStamped, velocity_callback)
+    pwm_pub = rospy.Publisher('/mavros/rc/override', OverrideRCIn, queue_size=10)
+    rospy.spin()
--- src/perception_suite/scripts/visual_servoing.py
+++ src/perception_suite/scripts/visual_servoing.py
@@ -2,34 +2,76 @@
 
 import rospy
 from std_msgs.msg import String
-from sensor_msgs.msg import LaserScan 
+from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
 disToObstacle = 1
 
-def callback(msg): 
-  rospy.loginfo(msg.data) #prints on terminal
 
-  # Controlller takes in the information from the boudning boxes from msg that it subscribes to
-  
-  ################# Implement Controller from Bounding Box to Velocity Here #################
-  
-  
-  
-  
-  
-  
-  ##############################################################
-  #Some PWM Command
-  pwm = 100
-  pub.publish(move)
-  
+def callback(msg):
+    #where msg is a list of tuple coordinates
+    #say max is 300 degrees per second
+    rospy.loginfo(msg.data)  # prints on terminal
+    max_x = 200
+    max_y = 200
 
-rospy.init_node('visual_servoing_node')
-sub = rospy.Subscriber('/perception/bounding_box_stuff', Twist, callback) #We subscribe to the boats velocity
-pub = rospy.Publisher('/visual_servoing/velocity', Twist, queue_size=2)
+    top_left_b1 = msg.data[0]
+    bottom_right_b1 = msg.data[1]
+
+    top_left_b2 = msg.data[2]
+    bottom_right_b2 = msg.data[3]
+
+    center_x_b1 = (top_left_b1[0] + bottom_right_b1[0])/2
+    center_y_b1 = (top_left_b1[1] + bottom_right_b1[1])/2
+
+    center_x_b2 = (top_left_b2[0] + bottom_right_b2[0])/2
+    center_y_b2 = (top_left_b2[1] + bottom_right_b2[1])/2
+
+    center_both_x = (center_x_b1 + center_x_b2)/2
+    center_both_y = (center_y_b1 + center_y_b2)/2
+
+    center_frame = (max_x/2, max_y/2)
+ 
+
+    #distances are negative when to the right of the center frame
+    #positive when to the left
+    dist_x = center_frame[0] - center_both_x
+    dist_y = center_frame[1] - center_both_y
+
+
+    # Controlller takes in the information from the boudning boxes from msg that it subscribes to
+
+    ################# Implement Controller from Bounding Box to Velocity Here #################
+
+    ##############################################################
+    # Some PWM Command
+    pwm = 100
+    pub.publish(move)
+
+
+
+rospy.init_node("visual_servoing_node")
+sub = rospy.Subscriber(
+    "/perception/bounding_box_stuff", Twist, callback
+)  # We subscribe to the boats velocity
+pub = rospy.Publisher("/visual_servoing/velocity", Twist, queue_size=2)
 rate = rospy.Rate(2)
 move = Twist()
+
+#want maximum to be 300 degrees(5.24 radians) per second if we are at
+#max distance(100 pixels)
+#want it to decrease linearly until we are some small distance away
+#when only 10 away, bring the velocity down to 0
+#negative turn rate corresponds to right turn
+#positive corresponds to left turn
+
+if abs(dist_x) > 10:
+    #will be negative(turn right) when dist_x is positive(too far left)
+    turn_speed = -3/2*dist_x*(3.14/180) #degrees to rads conversion
+else:
+    turn_speed = 0
+
+move.angular.x = turn_speed
 
 
 rospy.spin()
