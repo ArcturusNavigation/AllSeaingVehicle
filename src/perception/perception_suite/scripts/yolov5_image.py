545c3aa1b85d28bae36c22804d98027b7270ed77
Toya Takahashi <toyatakahashi522@gmail.com>
2023-07-14 01:59:04 -0400

    fix dataframe empty error

--- src/perception/perception_suite/scripts/yolov5_image.py
+++ src/perception/perception_suite/scripts/yolov5_image.py
@@ -17,33 +17,33 @@ from perception_suite.msg import LabeledBoundingBox2D, LabeledBoundingBox2DArray
 
 class Yolov5Detector():
     def __init__(self):
-        self.bridge = cv_bridge.CvBridge()
+        bridge = cv_bridge.CvBridge()
         rospack = rospkg.RosPack()
         
         # Get pretrained yolov5 model from local
         path_hubconfig = f"/home/{getpass.getuser()}/yolov5" # download yolov5 on the home directory
         path_trained_model = rospack.get_path("perception_suite") + "/model/buoy_detection_best_weights_v2.pt"
-        self.model = torch.hub.load(path_hubconfig, 'custom', path=path_trained_model, source='local')
+        model = torch.hub.load(path_hubconfig, 'custom', path=path_trained_model, source='local')
 
         # Model inference settings
-        # self.model.classes = [32] # Only detect sports ball
-        # self.model.conf = 0.05    # Confidence threshold
-        # self.model.iou = 0.25     # NMS IoU threshold (how close it is to an actual sports ball)
+        # model.classes = [32] # Only detect sports ball
+        # model.conf = 0.05    # Confidence threshold
+        # model.iou = 0.25     # NMS IoU threshold (how close it is to an actual sports ball)
 
         # Subscribers and publishers
-        self.pub = rospy.Publisher('/perception_suite/bounding_boxes', LabeledBoundingBox2DArray, queue_size=1)
-        self.img_pub = rospy.Publisher('/perception_suite/segmented_image', sensor_msgs.msg.Image, queue_size=1)
-
-        # Get image
-        self.img = PIL.Image.open(rospack.get_path("perception_suite") + "/images/test.jpg")
-        self.img = np.array(self.img.convert("RGB"))
+        pub = rospy.Publisher('/perception_suite/bounding_boxes', LabeledBoundingBox2DArray, queue_size=1)
+        img_pub = rospy.Publisher('/perception_suite/segmented_image', sensor_msgs.msg.Image, queue_size=1)
 
         # Publisher loop
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
 
+            # Get image
+            img = PIL.Image.open(rospack.get_path("perception_suite") + "/images/test.jpg")
+            img = np.array(img.convert("RGB"))
+
             # Detect buoys using yolov5
-            results = self.model(self.img)
+            results = model(img)
             preds = results.pandas().xyxy[0]  # Image 1 predictoins
             rospy.loginfo(preds)
             bboxes = LabeledBoundingBox2DArray()
@@ -63,11 +63,11 @@ class Yolov5Detector():
                 bbox.label = row['class']
                 bbox.probability = row['confidence']
                 bboxes.boxes.append(bbox)
-                cv2.rectangle(self.img, (bbox.min_x, bbox.min_y), (bbox.max_x, bbox.max_y), (255, 0, 0), 4)
+                cv2.rectangle(img, (bbox.min_x, bbox.min_y), (bbox.max_x, bbox.max_y), (255, 0, 0), 4)
 
             # Publish image and boudnign box
-            self.img_pub.publish(self.bridge.cv2_to_imgmsg(self.img, "rgb8"))
-            self.pub.publish(bboxes)
+            img_pub.publish(bridge.cv2_to_imgmsg(img, "rgb8"))
+            pub.publish(bboxes)
             rate.sleep()
 
 if __name__ == "__main__":
